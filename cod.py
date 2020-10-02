2afc535a63aad3a7fc41156d2065a764de4506e0
Keven-cmd <keven657@hotmail.com>
2020-10-01 22:19:39 -0300

    ac-03-devops

--- cod.py
+++ cod.py
@@ -12,9 +12,26 @@ def nao_entre_em_panico():
 
     return primos
 
-if _name_ == "_main_":
+if __name__ == "_main_":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
