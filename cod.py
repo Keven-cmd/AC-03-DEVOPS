d8d2ceae371bc1f9ce610ed4db19fc1692edc846
Keven Oliveira <69823750+Keven-cmd@users.noreply.github.com>
2020-10-15 15:08:35 -0300

    Update cod.py

--- cod.py
+++ cod.py
@@ -4,13 +4,32 @@ from math import sqrt
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def nao_entre_em_panico():
+def numeros_primos():
 
-    primos = "Tudo vai dar certo caros alunos! "
+    limite = 100
+    c = 1
+    inicio = 1
+    numero = 3
 
+    primos = "2, "
+
+    while inicio < limite:
+        ehprimo = 1
+        for i in range(2, numero):
+            if numero % i == 0:
+                ehprimo = 0
+                break
+        if (ehprimo):
+            primos = primos + str(numero) + ","
+            inicio += 1
+            if(inicio % 10 == 0):
+                primos = primos + "<br>"
+        numero += 1
 
     return primos
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
